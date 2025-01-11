309ed4199800eba29fa3750b5fb103d81ab07faf
Shuaib0111 <mohdshuaib77689@gmail.com>
2025-01-11 12:44:01 +0530

    updated db

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 
+console.log(process.env.USERNAME)
+console.log(process.env.PASSWORD)
 const connectToDB = ()=>{
-    mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@jobfinder.k4jkf.mongodb.net/?retryWrites=true&w=majority&appName=jobFinder`)
+    mongoose.connect(`mongodb+srv://mohdshuaib77689:${process.env.PASSWORD}@jobfinder.k4jkf.mongodb.net/?retryWrites=true&w=majority&appName=jobFinder`)
     .then(() => console.log('connected to mongodb successfully'))
-    .catch(()=>console.log('error in connecting mongodb'))
+    .catch((err)=>console.log(err))
 }
 
 module.exports = connectToDB
