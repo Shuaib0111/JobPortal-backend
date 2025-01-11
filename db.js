0baedff43d7b721337afb8ecaef0d14c9c0dd6e9
Shuaib0111 <mohdshuaib77689@gmail.com>
2025-01-11 11:40:02 +0530

    passwordCommit

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const connectToDB = ()=>{
-    mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${lWtk8XHgnkjmLRgl}@jobfinder.k4jkf.mongodb.net/?retryWrites=true&w=majority&appName=jobFinder`)
+    mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@jobfinder.k4jkf.mongodb.net/?retryWrites=true&w=majority&appName=jobFinder`)
     .then(() => console.log('connected to mongodb successfully'))
     .catch(()=>console.log('error in connecting mongodb'))
 }
