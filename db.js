a9e95796f0d60c191f30b4210652273c60011e9d
Shuaib0111 <mohdshuaib77689@gmail.com>
2025-01-11 11:16:38 +0530

    updated password

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const connectToDB = ()=>{
-    mongoose.connect('mongodb://127.0.0.1:27017/JobPortal_DB')
+    mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${lWtk8XHgnkjmLRgl}@jobfinder.k4jkf.mongodb.net/?retryWrites=true&w=majority&appName=jobFinder`)
     .then(() => console.log('connected to mongodb successfully'))
     .catch(()=>console.log('error in connecting mongodb'))
 }
